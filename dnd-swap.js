529985ff010fb7493e37c33bd2a49a14bff9eea0
Huỳnh Tuấn Anh <huynhtuananh21895@gmail.com>
2020-10-09 23:14:01 +0700

    Handle drop on ducument

--- dnd-swap.js
+++ dnd-swap.js
@@ -1,43 +1,54 @@
 console.log('DnD ex2 script loaded!!')
 
 window.addEventListener('load', function () {
+	let dragEnd = false
 	const dragItems = document.querySelectorAll('.ex3 .example-draggable')
 	dragItems.forEach(addDndEvents)
+
+	document.body.addEventListener('dragover', function(e) {
+		e.preventDefault()
+		console.log('Document dragover')
+	})
+	document.body.addEventListener('drop', function(e) {
+		console.log('Document drop')
+		if (!dragEnd) {
+			const dragElemId = e.dataTransfer.getData('text/plain')
+			document.getElementById(dragElemId).classList.remove('dragstart')
+			dragEnd = true
+		}
+	})
 
 	function addDndEvents(item) {
 		item.addEventListener('dragstart', function (e) {
 			console.log('Dnd - Drag start')
+			dragEnd = false
 			const target = e.target
-			e.dataTransfer.effectAllowed = 'move';
+			// e.dataTransfer.effectAllowed = 'move';
 			e.dataTransfer.setData('text/plain', e.target.id)
 			// e.dataTransfer.setDragImage(e.target, 0, 0)
 			target.classList.add('dragstart')
 		})
 
 		item.addEventListener('dragenter', function (e) {
-			e.preventDefault()
 			console.log('Dnd - Drag enter')
+			e.preventDefault()
 			const target = e.target
 			const dragElemId = e.dataTransfer.getData('text/plain')
 			if (target.id !== dragElemId) {
 				target.classList.add('dragover')
 			}
-
-			return true
 		})
 
 		item.addEventListener('dragleave', function (e) {
-			e.preventDefault()
 			console.log('Dnd - Drag leave')
+			e.preventDefault()
 			e.target.classList.remove('dragover')
 		})
 
 		item.addEventListener('dragover', function (e) {
-			e.preventDefault()
-			e.dropEffect = "move"
 			console.log('Dnd - dragover')
-
-			return false
+			e.preventDefault()
+			// e.dropEffect = "move"
 		})
 
 		item.addEventListener('drop', function (e) {
@@ -57,6 +68,7 @@ window.addEventListener('load', function () {
 			}
 			e.target.classList.remove('dragstart')
 			e.target.classList.remove('dragover')
+			dragEnd = true
 		})
 	}
 })
