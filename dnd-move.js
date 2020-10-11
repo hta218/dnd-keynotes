4e396fdde436595164141a4c309b4c7784494618
Huỳnh Tuấn Anh <huynhtuananh21895@gmail.com>
2020-10-11 09:40:23 +0700

    Add wrapper dragenter handler

--- dnd-move.js
+++ dnd-move.js
@@ -6,18 +6,36 @@ window.addEventListener('load', function () {
 	dragItems.forEach(addDndEvents)
 
 	function addDndEvents(item) {
-		item.addEventListener('dragstart', function(e) {
-			console.log('Dnd 2 - Drag start')
-			const id = e.target.id
+		item.addEventListener('dragstart', function (e) {
 			dragEl = e.target.cloneNode(true)
-			e.dataTransfer.setData('text/plain', id)
+			const wrapperIndex = e.target.parentNode.dataset.index
+			console.log('Dnd 2 - Drag start', wrapperIndex)
+			e.dataTransfer.setData('text/plain', wrapperIndex)
 			e.dataTransfer.effectAllowed = 'move'
 		})
 
-		item.parentNode.addEventListener('dragenter', function(e) {
+		item.parentNode.addEventListener('dragenter', function (e) {
 			console.log('Dnd 2 - Drag enter', this)
 			this.classList.add('over')
-			this.firstElementChild.classList.add('hide')
+
+			const currIndex = Number(this.dataset.index)
+			const wrapperIndex = Number(e.dataTransfer.getData('text/plain'))
+
+			if (currIndex !== wrapperIndex) {
+				const from = currIndex > wrapperIndex ? wrapperIndex : currIndex
+				const to = currIndex > wrapperIndex ? currIndex : wrapperIndex
+				const wrappers = document.querySelectorAll('.wrapper')
+				for (let i = from; i < to; i++) {
+					const wrapper = wrappers[i]
+					const next = wrappers[i + 1]
+					wrapper.firstElementChild.remove()
+					wrapper.appendChild(next.firstElementChild)
+				}
+			}
+		})
+
+		item.parentNode.addEventListener('dragleave', function(e) {
+
 		})
 	}
 })
