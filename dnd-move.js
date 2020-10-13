7c3e0384d7e0a73ca1256c1c18a280658edbdf27
Leo Huynh <huynhtuananh21895@gmail.com>
2020-10-13 17:17:16 +0700

    Update dragenter

--- dnd-move.js
+++ dnd-move.js
@@ -1,7 +1,10 @@
 console.log('DnD ex3 script loaded!!')
 
 window.addEventListener('load', function () {
-	let dragEl = null
+	const dragTracker = {
+		dragEl: null,
+		wrapperIndex: -1
+	}
 	const dragItems = document.querySelectorAll('.ex4 .example-draggable')
 	const wrappers = document.querySelectorAll('.wrapper')
 
@@ -10,16 +13,13 @@ window.addEventListener('load', function () {
 
 	function addWrapperDndEvents(wraper) {
 		wraper.addEventListener('dragenter', function (e) {
-			// e.preventDefault()
-			e.dropEffect = "move"
-			debugger
 			console.log('Dnd 2 - Drag enter', this.dataset.index)
 
 			const currIndex = Number(this.dataset.index)
-			const wrapperIndex = Number(e.dataTransfer.getData('text/plain'))
-			console.log('====> ', currIndex, e.dataTransfer.getData('text/plain'))
+			const wrapperIndex = Number(dragTracker.wrapperIndex)
+
 			if (currIndex !== wrapperIndex) {
-				this.firstElementChild.classList.add('hide')
+				// this.firstElementChild.classList.add('hide')
 				const from = currIndex > wrapperIndex ? wrapperIndex : currIndex
 				const to = currIndex > wrapperIndex ? currIndex : wrapperIndex
 
@@ -27,11 +27,11 @@ window.addEventListener('load', function () {
 				for (let i = from; i < to; i++) {
 					const wrapper = wrappers[i]
 					const next = wrappers[i + 1]
-					const nextChild = next.firstElementChild.cloneNode(true)
-					nextChild.classList.remove('hide')
+					// const nextChild = next.firstElementChild.cloneNode(true)
+					// nextChild.classList.remove('hide')
 					// console.log('Dnd 2 - Drag enter', next, i)
-					wrapper.firstElementChild.classList.add('hide')
-					wrapper.appendChild(nextChild)
+					// wrapper.firstElementChild.classList.add('hide')
+					wrapper.appendChild(next.firstElementChild)
 				}
 				// console.log('after for loop =====')
 			}
@@ -53,21 +53,22 @@ window.addEventListener('load', function () {
 			console.log('Dnd 2 - drop', this.dataset.index)
 			e.preventDefault()
 			this.firstElementChild.remove()
-			this.appendChild(dragEl)
+			this.appendChild(dragTracker.dragEl)
 		})
 	}
 
 	function addItemDndEvents(item) {
 		item.addEventListener('dragstart', function (e) {
-			dragEl = this.cloneNode(true)
+			dragTracker.dragEl = this.cloneNode(true)
+
 			const wrapperIndex = this.parentNode.dataset.index
-			console.log('Dnd 2 - Drag start', wrapperIndex)
 			e.dataTransfer.effectAllowed = 'move';
-			e.dropEffect = "move"
 			e.dataTransfer.setData('text/plain', wrapperIndex)
 
-			const test = e.dataTransfer.getData('text/plain')
-			console.log('====>', test)
+			// this.classList.add('hide')
+			dragTracker.wrapperIndex = wrapperIndex
+
+			console.log('Dnd 2 - Drag start', wrapperIndex)
 		})
 	}
 })
